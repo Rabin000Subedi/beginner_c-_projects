2cf28574b23b912358ec52627d776ab24b9aaece
Rabin Subedi <rabinsubedi@Rabins-MacBook-Air.local>
2026-07-26 19:24:06 +0545

    add guess the number game

---
+++ GuessNumber.cs
@@ -0,0 +1,55 @@
+﻿namespace GuessNumber
+{
+class Program
+    {
+        public static void Main(string[] args)
+        {
+        bool isCorrectguess = false;
+        Random random = new Random();
+        int randomNum = random.Next(1, 11);
+
+        System.Console.WriteLine("Welcome to the Guess the Number Game!" );
+        System.Console.WriteLine("Guess a number between 1 and 10: ");
+        System.Console.WriteLine("Guess the correct number to win the game!");
+
+        int attempts = 0;
+        while(!isCorrectguess)
+            {
+                System.Console.WriteLine("Enter your guess: ");
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (userGuess < randomNum)
+                {
+                    System.Console.WriteLine("Your guess is too low. Try again.");
+                    attempts++;
+                }
+                else if (userGuess > randomNum)
+                {
+                    System.Console.WriteLine("Your guess is too high. Try again.");
+                    attempts++;
+                }
+                else
+                {
+                    System.Console.WriteLine("Congratulations! You guessed the correct number!");
+                    isCorrectguess = true;
+                }
+            }
+            System.Console.WriteLine("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+            System.Console.WriteLine("Thank you for playing the Guess the Number Game!");
+
+        }}}
+
+        
+
+
+
+
+
+
+
+
+
+    
+    
+
+
